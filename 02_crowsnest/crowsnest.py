6b5664352de7a4e6ff0bb09cdf3143091cc6d73c
Nathan Moore <moore.2289@buckeyemail.osu.edu>
2024-09-18 00:43:33 -0400

    reformatted crowsnest.py

--- 02_crowsnest/crowsnest.py
+++ 02_crowsnest/crowsnest.py
@@ -2,7 +2,7 @@
 """
 Author : nathan <nathan@localhost>
 Date   : 2024-09-17
-Purpose: Rock the Casbah
+Purpose: Prints a message given a string argument
 """
 
 import argparse
@@ -13,30 +13,30 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Crow\'s Nest',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Crow's Nest",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('word',
-                        metavar='word',
-                        help='a word')
+    parser.add_argument("word", metavar="word", help="a word")
 
     return parser.parse_args()
 
 
 # --------------------------------------------------
 def main():
-    """Make a jazz noise here"""
+    """prints message"""
 
     args = get_args()
     word = args.word
     first_letter = word[0].lower()
 
-    article = 'a'
-    if first_letter in 'aeiou' :
-        article = 'an'
+    article = "a"
+    if first_letter in "aeiou":
+        article = "an"
 
-    print('Ahoy, Captain,', article, word, 'off the larboard bow!')
+    print("Ahoy, Captain,", article, word, "off the larboard bow!")
+
 
 # --------------------------------------------------
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
